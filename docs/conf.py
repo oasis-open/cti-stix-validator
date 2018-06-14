66eeb1af4332696e97f8a8d1d73197356ff4791c
Chris Lenk <clenk@mitre.org>
2018-06-14 11:04:03 -0400

    Bump version: 1.0.0 → 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,8 @@ project = 'stix2-validator'
 copyright = '2018, OASIS Open'
 author = 'OASIS Open'
 
-version = '1.0.0'
-release = '1.0.0'
+version = '1.0.1'
+release = '1.0.1'
 
 language = None
 exclude_patterns = ['_build', '_templates', 'Thumbs.db', '.DS_Store']
