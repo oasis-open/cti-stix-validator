4cd14e62eea5b2fe492a47d39bf468f3b6f45354
Chris Lenk <clenk@mitre.org>
2020-06-10 07:59:34 -0400

    Allow 2.0 objects in 2.1 bundles

--- stix2validator/v20/shoulds.py
+++ stix2validator/v20/shoulds.py
@@ -13,7 +13,7 @@ To add a new check:
     - add the check code and name to table
 """
 
-from collections import Iterable
+from collections.abc import Iterable
 from itertools import chain
 import re
 
